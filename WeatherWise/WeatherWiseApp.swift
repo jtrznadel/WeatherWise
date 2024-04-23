c103af77a7a1a32d596e21f357a54179597f2d48
jtrznadel <trznadeljakub3ai@gmail.com>
2024-04-23 17:36:44 +0200

    Initial Commit

---
+++ WeatherWise/WeatherWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWiseApp.swift
+//  WeatherWise
+//
+//  Created by Jakub Trznadel on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
